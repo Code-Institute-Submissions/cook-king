846520f2ade3aeb5c8a0b626b2c95326f68e7e5e
Douglas Donohue <falloutelite@gmail.com>
2019-04-09 16:29:07 +0000

    still fixing it

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import os
 from flask import Flask
+
 app = Flask(__name__)
-# app.config["MONGO_DBNAME"] = 'cooking'
-# app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://root:goodbowie44@myfirstcluster-milxz.mongodb.net/cooking?retryWrites=true')
-# # mongo = PyMongo(app)
+
 
 @app.route('/')
 def hello():
-    return "hello world"
+    return 'Hello World ...again'
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
