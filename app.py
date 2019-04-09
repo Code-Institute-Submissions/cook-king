e2c3d92bf67d90f6d4285e3979a4d5fd5aedc3b1
Douglas Donohue <falloutelite@gmail.com>
2019-04-09 16:36:17 +0000

    should be working now

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-
+app.config["MONGO_DBNAME"] = 'cooking'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://root:goodbowie44@myfirstcluster-milxz.mongodb.net/cooking?retryWrites=true')
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
-
-
+@app.route('/recipes')
+def recipes():
+    return render_template("recipes.html", 
+                           tasks=mongo.db.recipes.find())
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
