95daf1a1a6377644b068a392a1acc388567f57ba
Douglas Donohue <falloutelite@gmail.com>
2019-04-10 18:36:31 +0000

    added a random secret key

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ app.config["MONGO_DBNAME"] = 'cooking'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://root:goodbowie44@myfirstcluster-milxz.mongodb.net/cooking?retryWrites=true')
 mongo = PyMongo(app)
 
+class BaseObject(object):
+    app.secret_key='%\xdf\xca*\x03\xf3\xdf3\xf6)\xe31\xcd\xbb)\x17'
 @app.route('/')
 @app.route('/recipes')
 def recipes():
@@ -15,7 +17,7 @@ def recipes():
     return render_template("recipes.html", 
                            tasks=mongo.db.recipes.find())
     
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     users = mongo.db.users
     user_login = users.find.one({'name' : request.form['username']})
